21e96437b3821a5e760036455112fff2fe0fda55
Leevie Martin <leevie@gmail.com>
2019-02-15 00:01:35 -0600

    Updated, Added Style, Structure

--- unit-4-game/assets/javascript/game.js
+++ unit-4-game/assets/javascript/game.js
@@ -1,8 +1,11 @@
-var titleHead = document.getElementById("titleDiv");
-titleHead.innerHTML = "Crystal Collectors Game";
+// var titleHead = document.getElementById("titleDiv");
+// titleHead.innerHTML = "Crystal Collectors Game";
+$("#titleDiv").html("Crystal Collectors Game"); //does the same as above using jQuery!
 
-var instructBlock = document.getElementById("instructDiv");
-instructBlock.innerHTML = "These are the instructions";
+// var instructBlock = document.getElementById("instructDiv");
+// instructBlock.innerHTML = "These are the instructions";
+$("#instructDiv").html("These are the instructions"); 
 
 
 $(".targetNumDiv").html("Player Score");
+
