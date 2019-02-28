b2a72f74bef45ec89ed32e19a3392ce73fb16e8c
Leevie Martin <leevie@gmail.com>
2019-02-28 06:38:41 -0600

    Updated Repo

--- triviaGame/assets/javascript/app.js
+++ triviaGame/assets/javascript/app.js
@@ -15,7 +15,7 @@ var timeCounter = 15;
 var scoreCounter = 0;
 var questionCounter = 0;
 
-var myVar = setTimeout(iterateQuestion, 3000);
+// var myVar = setInterval(iterateQuestion, 3000);
 
 // Game Start Function
 var iterateQuestion = function() {    
@@ -64,19 +64,12 @@ var askQuestions = function(someObj) {
         for(var j = 0; j < someObj.answers.length; j++) {
             ansText = $("<li>");
             ansText.addClass("list-group-item");
-            ansText.attr("answer-value", ansLtrArray[j]);
+            ansText.attr("answer-value", someObj.answers[j]);
             $(".list-group").append(ansText);
             ansText.html(someObj.answers[j]);
         }
-        // function that asks for input here?  returns result and passed to compareFunc.
-            // what is the user's answer?  if 
-        // $(document).keyup(function(event){
-        //     a = event.key;
-        //     userAnswer = a.toUpperCase();
-        //     });
-    
-        // userAnswer = "C";  //for example --Assign on.click value here (user's input)
-        // console.log("What is userAnswer:  " + userAnswer);
+
+
         console.log("correctAns is:  " + correctAns);
 
         
@@ -95,18 +88,15 @@ var askQuestions = function(someObj) {
         b = correctAns;  //need to figure out how to get correct answer here.
         if(a === b) {
             scoreCounter++;
-            questionCounter++;
-            alert("The Force is Strong with this one...");
+            $("#ansDiv").html("<h2>The Force is strong with you..<h2>");
         } else {
-            alert("You've failed me for the last time, Admiral...");
-            // questionCounter++;
+            $("#ansDiv").html("You've failed me for the last time, Admiral...");
         }
         console.log(a === b);
     iterateQuestion();
     };
 
 var questArray = [
-    // questionOneFunc = function() 
        {
             question: "Which Star Wars movie &nbsp;<i>SHOULD</i>&nbsp; always be viewed first for a newcomer?",
             answers: [
@@ -138,110 +128,81 @@ var questArray = [
             correctAns: function() {return this.answers[2]}              
         },
 
-        // questObjFour = {
-        //     question: "What is the Empire's 'Secret Weapon' called?",
-        //     answers: [
-        //         "AT-AT Walker",
-        //         "Star Destroyer",
-        //         "The Millenium Falcon",
-        //         "The Death Star"
-        //     ],
-        //     correctAns: function() {return this.answers[3]}              
-        // },
-        // questObjFive = {
-        //     question: "Who is the Evil Emperor?",
-        //     answers: [
-        //         "Darth Vader",
-        //         "Count Dooku",
-        //         "Darth Sidious",
-        //         "General Grievous"
-        //     ],
-        //     correctAns: function() {return this.answers[2]}              
-        // },
-        // questObjSix = {
-        //     question: "What cold, snowy planet was home to a famous rebel base?",
-        //     answers: [
-        //         "Hoth",
-        //         "Tatooine",
-        //         "Coruscant",
-        //         "Yavin 4"
-        //     ],
-        //     correctAns: function() {return this.answers[0]}              
-        // },
-        // questObjSeven = {
-        //     question: "What color is Darth Vader's Lightsaber?",
-        //     answers: [
-        //         "Orange",
-        //         "Green",
-        //         "Gold",
-        //         "Red"
-        //     ],
-        //     correctAns: function() {return this.answers[3]}              
-        // },
-        // questObjEight = {
-        //     question: "What is the name of Darth Vader's grandson?",
-        //     answers: [
-        //         "Luke Skywalker",
-        //         "Kylo Ren",
-        //         "Han Solo",
-        //         "Obi Wan Kenobi"
-        //     ],
-        //     correctAns: function() {return this.answers[1]}              
-        // },
-        // questObjNine = {
-        //     question: "Who is Luke Skywalker's twin sister?",
-        //     answers: [
-        //         "Jyn Erso",
-        //         "Rey",
-        //         "General Leia Organa",
-        //         "Rose"
-        //     ],
-        //     correctAns: function() {return this.answers[2]}              
-        // },
-        // questObjTen = {
-        //     question: "What ship is famous for it's 'Kessel Run' in under 12 parsecs? ",
-        //     answers: [
-        //         "Tie Fighter Advanced",
-        //         "The Millenium Falcon",
-        //         "The X-Wing Fighter",
-        //         "The Star Destroyer"
-        //     ],
-        //     correctAns: function() {return this.answers[1]}              
-        // },
+        questObjFour = {
+            question: "What is the Empire's 'Secret Weapon' called?",
+            answers: [
+                "AT-AT Walker",
+                "Star Destroyer",
+                "The Millenium Falcon",
+                "The Death Star"
+            ],
+            correctAns: function() {return this.answers[3]}              
+        },
+        questObjFive = {
+            question: "Who is the Evil Emperor?",
+            answers: [
+                "Darth Vader",
+                "Count Dooku",
+                "Darth Sidious",
+                "General Grievous"
+            ],
+            correctAns: function() {return this.answers[2]}              
+        },
+        questObjSix = {
+            question: "What cold, snowy planet was home to a famous rebel base?",
+            answers: [
+                "Hoth",
+                "Tatooine",
+                "Coruscant",
+                "Yavin 4"
+            ],
+            correctAns: function() {return this.answers[0]}              
+        },
+        questObjSeven = {
+            question: "What color is Darth Vader's Lightsaber?",
+            answers: [
+                "Orange",
+                "Green",
+                "Gold",
+                "Red"
+            ],
+            correctAns: function() {return this.answers[3]}              
+        },
+        questObjEight = {
+            question: "What is the name of Darth Vader's grandson?",
+            answers: [
+                "Luke Skywalker",
+                "Kylo Ren",
+                "Han Solo",
+                "Obi Wan Kenobi"
+            ],
+            correctAns: function() {return this.answers[1]}              
+        },
+        questObjNine = {
+            question: "Who is Luke Skywalker's twin sister?",
+            answers: [
+                "Jyn Erso",
+                "Rey",
+                "General Leia Organa",
+                "Rose"
+            ],
+            correctAns: function() {return this.answers[2]}              
+        },
+        questObjTen = {
+            question: "What ship is famous for it's 'Kessel Run' in under 12 parsecs? ",
+            answers: [
+                "Tie Fighter Advanced",
+                "The Millenium Falcon",
+                "The X-Wing Fighter",
+                "The Star Destroyer"
+            ],
+            correctAns: function() {return this.answers[1]}              
+        },
 
 
     ]
     
-    //Need to get the result of their guess here:
 
-
-    // Function to compare result to correctAns
-
-    
-//     questionTwoFunc = function() {
-        
-//     },
-    
-//     questionThreeFunc = function() {
-        
-//     }
-// ];
-
-// var questObjOne = {
-//     question: "Which Star Wars movie <i> SHOULD </i> always be viewed first for a newcomer?",
-//     answer1: "Episode 1: The Phantom Menace",
-//     answer2: "Episode 6: Return of the Jedi",
-//     answer3: "Episode 4: A New Hope",
-//     answer4: "Episode 7: The Force Awakens",
-//     correctAns: function() {return this.answer3}
-
-// }
-
-// var someFunc = function(a,b) {
-//     if(a === b) {
-//         $("#ansDiv").html("YES!!");
-//     }
-// }
 
 
 // Console Logs
