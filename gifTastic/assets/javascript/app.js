670f047b74a6c7d17664e1e9b029cd279b58966d
Leevie Martin <leevie@gmail.com>
2019-03-02 22:37:33 -0600

    Updated GifTastic HW

--- gifTastic/assets/javascript/app.js
+++ gifTastic/assets/javascript/app.js
@@ -2,7 +2,6 @@
 
 
 
-
 var topics = ["Belle", "Ariel", "Princess Aurora", "Snow White", "Rapunzel", "Gaston", "Malefacent", "Ravenna", "Mother Gothel"];
 // Creates the Buttons
 for (var i = 0; i < topics.length; i++) {
@@ -14,24 +13,27 @@ for (var i = 0; i < topics.length; i++) {
 };
 
 // Performs actions to search for gifs when clicked
-$("#gif-div-btn").on("click", function () {
+$(".btn").on("click", function () {
+    $("#gif-div").html("");  // This should clear the DIV each time so only one set is displayed
+
     var myKey = "BGLn1MChCH92a5eM0yluChMk89OWrItU";
-    var searchVar ="Malefacent"//$(this).attr("gif-name");  // Using 'tag' to search, may switch to 'search' URL and use 'q'(query) if results undesirable
+    var searchVar = $(this).attr("gif-name");  // Using 'tag' to search, may switch to 'search' URL and use 'q'(query) if results undesirable
     var ratingVar = "G";
     var limitVar = 10;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + myKey + "&q=" + searchVar + "&rating=" + ratingVar + "&limit=" + limitVar;
+    var offsetVar = Math.floor(Math.random() * 15);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + myKey + "&q=" + searchVar + "&limit=" + limitVar + "$offset=" + offsetVar  + "&rating=" + ratingVar;
 
-
+    console.log("This is offsetVar:  " + offsetVar);
+    console.log("This is searchVar:  " + searchVar);
     $.ajax({
         url: queryURL,
         method: "GET"
     })
     .then(function (response) {
-        console.log(queryURL);
+        console.log(queryURL);  // console.logging the URL
         console.log(response);
         var results = response.data;
         console.log(results);
-            //    $("#gif-div").html("");  // This should clear the DIV each time so only one set is displayed
 
 
             for(var d = 0; d < results.length; d++) {
