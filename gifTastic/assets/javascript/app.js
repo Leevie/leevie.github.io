8e93ab7d745076db7edb4a30e36c8a134ffd828f
Leevie Martin <leevie@gmail.com>
2019-03-05 23:32:50 -0600

    Updated HW -- working

--- gifTastic/assets/javascript/app.js
+++ gifTastic/assets/javascript/app.js
@@ -13,9 +13,11 @@ for (var i = 0; i < topics.length; i++) {
     $("#gif-div-btn").append(div);
 };
 
-$("#gif-search").on("click", function(event) {  // Need a different event versus 'click'
+$(document).on("click", ".gif-click", function(event) {  // Need a different event versus 'click'  // **ISSUE "enter" button seems to 'refresh' the page.
     event.preventDefault();
-    var a = $(this).val().trim();
+
+    console.log("I made it to SEARCH");
+    var a = $("#gif-search").val().trim();
     topics.push(a);
     console.log(topics);
     // may need to Create this as a function
@@ -27,19 +29,17 @@ $("#gif-search").on("click", function(event) {  // Need a different event versus
             .text(topics[i]);
         $("#gif-div-btn").append(div);
     };
-    //////
-
 });
 
 // Performs actions to search for gifs when clicked, display them to the screen.
-$(".btn").on("click", function () {
+$(document).on("click", ".btn", function () {
     $("#gif-div").html("");  // This should clear the DIV each time so only one set is displayed
 
     var myKey = "BGLn1MChCH92a5eM0yluChMk89OWrItU";
     var searchVar = $(this).attr("gif-name");  // Using 'tag' to search, may switch to 'search' URL and use 'q'(query) if results undesirable
-    var ratingVar = "G";
+    var ratingVar = "PG";
     var limitVar = 10;
-    var offsetVar = Math.floor(Math.random() * 15);
+    var offsetVar = Math.floor(Math.random() * 10);
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + myKey + "&q=" + searchVar + "&limit=" + limitVar + "&offset=" + offsetVar  + "&rating=" + ratingVar;
 
     console.log("This is offsetVar:  " + offsetVar);
@@ -60,17 +60,17 @@ $(".btn").on("click", function () {
             var gifCard = $("<div>")
             .addClass("card")
             .attr("id", "gif-card")
-            .attr("style", "width: auto;");
+            .attr("style", "width: auto;")
+            .attr("style", "height: auto");
             // .text("This is some text");
 
             var gifImage = $("<img>")
-            // .attr("src", results[d].images.fixed_height_small_still.url) // STATIC (default state) image URL string of object
-            .attr("src", results[d].images.fixed_height_small.url) // ANIMATED (to show Gavin)
+            .attr("src", results[d].images.fixed_height_small_still.url) // STATIC (default state) image URL string of object
+            // .attr("src", results[d].images.fixed_height_small.url) // ANIMATED (to show Gavin)
             .attr("gif-animated", results[d].images.fixed_height_small.url) // ANIMATED image URL string of object
             .attr("gif-static", results[d].images.fixed_height_small_still.url) // STATIC image URL string of object
-            .attr("gif-state", "animated") // reflects the state of the .gif
-            .attr("id", "gifImage")
-            .addClass("gifImages");
+            .attr("gif-state", "static") // reflects the state of the .gif
+            .addClass("gif-image");
             $("#gif-div").append(gifCard) 
             gifCard.append(gifImage);
             gifCard.append("<div class='card-body'>" + "<p class='card-text'>" + "Rated:  " + results[d].rating.toUpperCase() + "</p>");  // Evil **** Genius!!
@@ -85,10 +85,10 @@ $(".btn").on("click", function () {
         
 });
 
-$(document).on("click",".gifImages", function() {  // (".gif") didn't work, trying ("<img>")-- didn't work, trying an id--DIDN'T WORK!  revisit
+$(document).on("click",".gif-image", function() {  // (".gif") didn't work, trying ("<img>")-- didn't work, trying an id--DIDN'T WORK!  revisit
 console.log("I MADE IT TO 'CLICK'");
 
-var state = $("#gifImage").attr("gif-state");
+var state = $(this).attr("gif-state");
 
 
 if(state === "static") {  // If static, change the URL to the animated .gif, change the attr 'state' to animated
