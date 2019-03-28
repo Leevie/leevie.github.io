f31bbed4927e146d64d6a55d9975ff1e65e27248
Leevie Martin <leevie@gmail.com>
2019-03-27 23:45:35 -0500

    Updated Portfolio

---
+++ portfolio/assets/javascript/tools.js
@@ -0,0 +1,7 @@
+var inputFile = process.argv[2];
+
+var fs = require("fs");
+
+fs.readFile()
+
+// attempting to write a Node function that takes in input, writes to a file.  Re-visit when time permits.  // use-case: pulling in "min.css" files, etc. -- maybe "axios"?
