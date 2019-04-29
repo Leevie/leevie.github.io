a2e8039c03b1f4d1d20baa561483ecc0e428ced1
Leevie Martin <leevie@gmail.com>
2019-04-28 23:03:58 -0500

    setting up structure

---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Whopper", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Mac-er", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Waffle Burger", FALSE);
