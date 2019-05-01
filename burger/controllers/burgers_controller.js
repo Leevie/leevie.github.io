08a9a21cd61a0c9bd93c6592dfa3d984bccbff10
Leevie Martin <leevie@gmail.com>
2019-05-01 07:22:18 -0500

    Updated folder structure

---
+++ burger/controllers/burgers_controller.js
@@ -0,0 +1,2 @@
+var express = require("express");
+require("../models/burger");
---
+++ burger/models/burger.js
@@ -0,0 +1,9 @@
+require("../config/orm");
+
+
+// Also inside burger.js, create the code that will call the ORM functions using 
+//burger specific input for the ORM.
+
+
+
+module.exports = [allfunctions]
