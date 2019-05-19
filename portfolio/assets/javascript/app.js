948e8ac3faabdee40d7d5da00dea39a8e00db1b6
Leevie Martin <leevie@gmail.com>
2019-05-18 21:14:51 -0500

    unspecified changes

--- portfolio/assets/javascript/app.js
+++ portfolio/assets/javascript/app.js
@@ -8,3 +8,64 @@ if ($('.text-slider').length == 1) {
 			backSpeed: 30
 		});
 	}
+	var myConfig = {
+    graphset: [
+      {
+        type: "wordcloud",
+        options: {
+          style: {
+            tooltip: {
+              visible: true,
+              text: "%text: %hits"
+            }
+          },
+          words: [
+            {
+              text: "Javascript",
+              count: "342"
+            },
+            {
+              text: "NodeJs",
+              count: "296"
+            },
+            {
+              text: "HTML5",
+              count: "212"
+            },
+            {
+              text: "jQuery",
+              count: "201"
+            },
+            {
+              text: "MySQL",
+              count: "183"
+            },
+            {
+              text: "Bootstrap",
+              count: "130"
+            },
+            {
+              text: "Express",
+              count: "125"
+            },
+            {
+              text: "CSS",
+              count: "121"
+            },
+            {
+              text: "Git",
+              count: "117"
+            }
+          ]
+        }
+      }
+    ]
+  };
+
+  zingchart.render({
+    id: "myChart",
+    data: myConfig,
+    height: "100%",
+    width: "100%"
+	});
+	
