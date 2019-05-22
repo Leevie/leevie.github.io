e464913bd4b6f7ceb91c42d868a1b27232f57279
Leevie Martin <leevie@gmail.com>
2019-05-22 00:31:03 -0500

    cleaning up repo, moving projects to own repo

--- burger/db/seeds.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Big Whopper", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("The Mac-er", FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Waffle Burger", FALSE);
--- burger/models/burger.js
+++
@@ -1,9 +0,0 @@
-require("../config/orm");
-
-
-// Also inside burger.js, create the code that will call the ORM functions using 
-//burger specific input for the ORM.
-
-
-
-module.exports = [allfunctions]
